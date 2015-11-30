066fe97a8b4506f227fad1ac69dafd50147e06a0
elvismdev <elvismdev@gmail.com>
2015-11-30 05:36:33 -0500

    Top sellers report

--- www/js/config.js
+++ www/js/config.js
@@ -3,7 +3,6 @@ var store = {
     'name': 'MyGCTV.com',
     'http_method': 'https://',
     'domain': 'grantcardonetv.com',
-    'version': 'v2',
     'consumer_key': 'ck_b0fd7ac1322e8e23dd0775da34b6e899', // From WP user profile configuration
     'customer_secret': 'cs_fd46d7d0fff710cc0d5f46134cc2cb34', // From WP user profile configuration
     /*'customer_secret': 'cs_fd46d7d0fff710cc0d5f46134cc2cb34' + '&', // From WP user profile configuration (v3) Also change in version and request parameters for v3*/
@@ -17,7 +16,6 @@ var gcStore = {
     'name': 'GC.com',
     'http_method': 'http://',
     'domain': 'www.grantcardone.com',
-    'version': 'v2',
     'customer_secret': 'cs_cefb91e1ea19a54a18c085702b0fe8b1', // From WP user profile configuration
     'filter': '?filter[created_at_min]=',
     'oauth': {
