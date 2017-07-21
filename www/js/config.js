d50f4ae33520877ba00b227253c04d5b9f8a28da
Elvis  Morales <elvismdev@gmail.com>
2017-07-21 16:54:07 +0000

    config.js edited online with Bitbucket

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
-// Store.com
+// wcstore1.com
 var store = {
-    'name': 'MyGCTV.com',
+    'name': 'wcstore1.com',
     'http_method': 'https://',
-    'domain': 'shop.grantcardonetv.com',
+    'domain': 'shop.wcstore1.com',
     'consumer_key': 'ck_d18375c60c47b06b3f57e6d36b23210a15668462', // From WP user profile configuration
     'customer_secret': 'cs_61b9c07878ab6ecb71c4208c797e64c1a8b8ba67', // From WP user profile configuration
     'filter': '?filter[created_at_min]=',
@@ -10,11 +10,11 @@ var store = {
 };
 
 
-// GC.com
+// wcstore2.com
 var gcStore = {
-    'name': 'GC.com',
+    'name': 'wcstore2.com',
     'http_method': 'https://',
-    'domain': 'www.grantcardone.com',
+    'domain': 'www.wcstore2.com',
     'consumer_key': 'ck_ac46a54f69559bd03dc5c659f3753bb8', // From WP user profile configuration
     'customer_secret': 'cs_cefb91e1ea19a54a18c085702b0fe8b1', // From WP user profile configuration
     'filter': '?filter[created_at_min]=',
